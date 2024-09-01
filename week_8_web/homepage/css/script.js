ca5fbe2a630020988927db318b696b52f0b9262e
nzyoka10 <mnzyokaeric@gmail.com>
2024-09-01 16:00:42 +0300

    project homepage

---
+++ week_8_web/homepage/css/script.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Welcome to My Website!");
+});
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     Swal.fire({
+//         title: 'Hello.',
+//         text: 'Welcome to My Website!',
+//         icon: 'success',
+//         confirmButtonText: 'OK'
+//     });
+// });
+
